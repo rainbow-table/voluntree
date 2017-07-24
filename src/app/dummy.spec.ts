96feee5200d7f6418360da545a30a117722b44c3
tournetorres <tournetorres@gmail.com>
2017-07-24 18:23:55 -0500

    (fix) fixed it again

---
+++ src/app/dummy.spec.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+
+// let expect = chai.expect;
+describe('Dummy test', () => {
+  return it('should crash', () => {
+    return expect(false).to.be.false;
+  });
+});
--- src/dummy.spec.js
+++
@@ -1,6 +0,0 @@
-let expect = chai.expect;
-describe('Dummy test', () => {
-  return it('should crash', () => {
-    return expect(true).to.be.false;
-  });
-});
