def8155bc072ffa07c9492e930c9ee6fd576accc
Daniel Weill <daniel.zalmen@gmail.com>
2017-07-28 17:56:38 -0500

    (update) exclude outside events

--- src/pages/manage-events/manage-events.ts
+++ src/pages/manage-events/manage-events.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { GrabNpEventsProvider } from '../../providers/grab-np-events/grab-np-events';
+import { NpCalProvider } from '../../providers/np-cal/np-cal';
 import { Storage } from '@ionic/storage';
 
 /**
@@ -13,24 +14,33 @@ import { Storage } from '@ionic/storage';
 @Component({
   selector: 'page-manage-events',
   templateUrl: 'manage-events.html',
-
+  providers: [NpCalProvider]
 })
 export class ManageEventsPage {
   npevents: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public GrabNpEventsProvider: GrabNpEventsProvider, public storage: Storage) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public GrabNpEventsProvider: GrabNpEventsProvider, public NpCalProvider: NpCalProvider, public storage: Storage) {
   }
-
-  ionViewDidLoad() {
+  public id;
+  async ionViewDidLoad() {
+    this.id = await this.storage.get('id');    
     this.loadNpEvents()
     console.log('ionViewDidLoad ManageEventsPage');
   }
 
   loadNpEvents(){
-      this.GrabNpEventsProvider.load()
-      .then(data => {
-        this.npevents = data.data.event;
-    });
+      this.NpCalProvider.getCalEvents({query: `{event(ngo_id: ${this.id}){
+            id
+            ngo_id
+            description
+            event_start
+            event_end
+            event_address
+        }}`
+        }).then(response => {
+          this.npevents = response.event;           
+        });
+
   }
 
   removeEventFromDB(event_id) {
